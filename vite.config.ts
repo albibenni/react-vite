84ee100dff64a4ec2e3bc755095bf106ad3c8539
Alberto Benatti <albi.benni8@gmail.com>
2023-04-23 17:47:52 +0200

    plugins: svgr and checker with typescript enabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteSvgr from "vite-plugin-svgr";
+import checker from "vite-plugin-checker";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSvgr(), checker({
+    typescript: true
+  })],
 })
