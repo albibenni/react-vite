2da2a4be42e525a55280de7ecac78293e81ea47e
Alberto Benatti <albi.benni8@gmail.com>
2023-05-14 17:24:36 +0200

    store like course

--- src/store/store.ts
+++ src/store/store.ts
@@ -31,32 +31,47 @@
 // //   unknown,
 // //   Action
 // // >;
-// import { configureStore, combineReducers, ThunkAction } from '@reduxjs/toolkit';
-// import { Action } from 'vite-plugin-checker/dist/cjs/types';
-// import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-//
-// export const store = configureStore({
-//   reducer: combinedReducer,
-//   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-// });
-//
-// const combinedReducer = combineReducers({});
-// // Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>;
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch;
-//
-// // Use throughout your app instead of plain `useDispatch` and `useSelector`
-// export const useAppDispatch: () => AppDispatch = useDispatch;
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-//
-// // type RootState = ReturnType<typeof combinedReducer>;
-// export type AppThunk<ReturnType = void> = ThunkAction<
-//   ReturnType,
-//   RootState,
-//   unknown,
-//   Action
-// >;
-// export default {
-//   store,
-// };
+import {
+  configureStore,
+  combineReducers,
+  ThunkAction,
+  Action,
+  PayloadAction,
+} from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+const counterReducer = (state: RootState, action: PayloadAction) => ({
+  counter: state.counter + 1,
+});
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+export const store = configureStore({
+  reducer: counterReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+});
+
+store.subscribe(counterSubscriber);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// type RootState = ReturnType<typeof combinedReducer>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action
+>;
+export default {
+  store,
+};
