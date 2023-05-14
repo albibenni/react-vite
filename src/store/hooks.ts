fea64e175ef41328bd4e66dfcd6c037096f76583
Alberto Benatti <albi.benni8@gmail.com>
2023-05-14 10:41:17 +0200

    wip store

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,9 +1,9 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from './store';
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-// @ts-ignore
-export function useParamSelector(selector, ...params) {
-  return useSelector((state: any) => selector(state, ...params));
-}
+// import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+// import { AppDispatch, RootState } from './store';
+//
+// export const useAppDispatch: () => AppDispatch = useDispatch;
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// // @ts-ignore
+// export function useParamSelector(selector, ...params) {
+//   return useSelector((state: any) => selector(state, ...params));
+// }
