2a7dcf951b5cb00ad7bf94f996fa2efc963bc07c
Alberto Benatti <albi.benni8@gmail.com>
2023-05-16 08:49:13 +0200

    wip ref

--- src/search/pages/Search.tsx
+++ src/search/pages/Search.tsx
@@ -1,13 +1,36 @@
 import '../../App.css';
+import React, { useRef, useState } from 'react';
 
 function Search() {
+  const [items, setItems] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const prefilled = () => {
+    setItems((prevState) => [...prevState, 'John', 'Jane', 'Mike']);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { value } = inputRef.current;
+    if (value) return;
+    setItems((prevState) => [...prevState, value]);
+  };
+
   return (
     <div className="app">
+      <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRef} type="text" />
+        <button type="submit">Add </button>
+      </form>
       <input type="text" className="search" placeholder="Search" />
+      <button type="submit" onClick={prefilled}>
+        {' '}
+        Prefilled{' '}
+      </button>
       <ul className="List">
-        <li className="listItem">John</li>
-        <li className="listItem">Jane</li>
-        <li className="listItem">Mike</li>
+        {items.map((item) => (
+          <li className="listItem">{item}</li>
+        ))}
       </ul>
     </div>
   );
